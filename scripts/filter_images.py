0d0c155510864a786d910c7b6f4d8283598aec6f
Martin van Harmelen <Martin@vanharmelen.com>
2015-05-18 17:12:55 +0200

    Improced output formating of filter_images

--- scripts/filter_images.py
+++ scripts/filter_images.py
@@ -4,11 +4,12 @@
 
 from copy import deepcopy
 
-def filter_data(data, filter_key=lambda row: 'Image' in row):
+def filter_data(data, key=lambda row: 'Image' in row):
     """
-    Take data formatted as dumper by kimonolabs and remove rows without an image.
+    Take data formatted as dumped by kimonolabs and remove rows without an image.
 
     Assume there is only one collection
+    Update count
     """
     # Make sure we don't muck up the data that was given
     data = deepcopy(data)
@@ -19,7 +20,7 @@ def filter_data(data, filter_key=lambda row: 'Image' in row):
 
     # Filter
     rows = collections[collection_name]
-    collections[collection_name] = [row for row in rows if filter_key(row)]
+    collections[collection_name] = [row for row in rows if key(row)]
     data['count'] = len(collections[collection_name])
     return data, (data['count'], len(rows))
 
@@ -28,7 +29,8 @@ if __name__ == '__main__':
     import json
     import argparse
     parser = argparse.ArgumentParser(description='Filter usable json data')
-    parser.add_argument('input_filename', metavar='input file', nargs=1, help='File to read json from')
+    parser.add_argument('input_filename', metavar='input file', nargs=1,
+                        help='File to read json from')
     parser.add_argument('output_filename', metavar='output file', nargs='?',
                         help='File to output filtered json to')
     args = parser.parse_args()
@@ -49,6 +51,7 @@ if __name__ == '__main__':
 
     # Write
     with open(output_filename, 'w') as filedescriptor:
-        json.dump(clean_data, filedescriptor)
+        # Indent with 4 spaces
+        json.dump(clean_data, filedescriptor, indent=4)
 
     print('Found {} of {} rows with images.'.format(*info))
